cff389612f8e8a0defd92f63ec86c5239982140d
Ahmed <tanvirahmed754575@gmail.com>
2026-02-14 22:09:33 +0600

    project setup

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Create main folders
+mkdir -p project
+mkdir -p project/cloud
+mkdir -p project/components
+mkdir -p project/constants
+mkdir -p project/pipeline
+mkdir -p project/entity
+mkdir -p project/logger
+mkdir -p project/exception
+mkdir -p project/utils
+
+# # Create __init__.py in each folder
+touch project/__init__.py
+touch project/cloud/__init__.py
+touch project/components/__init__.py
+touch project/constants/__init__.py
+touch project/pipeline/__init__.py
+touch project/entity/__init__.py
+touch project/logger/__init__.py
+touch project/exception/__init__.py
+touch project/utils/__init__.py
+
+# Create main files
+touch app.py
+touch requirements.txt
+touch notex.txt 
+touch setup.py 
+touch setup.sh
+
+
+
+
+## bash setup.sh
+
+
